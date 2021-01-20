cc29485ec911da31010c106bc3ab8fdb2a19ddbf
Realeboga Dikole <30493588+readikole@users.noreply.github.com>
2021-01-20 17:32:22 +0200

    Add files via upload

--- Rayleigh-Ritz Method.py
+++ Rayleigh-Ritz Method.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-'''I use the Rayleigh-Ritz method to solve a Sturm-Lioville differential equation with a nonzero RHD
+'''I use the Rayleigh-Ritz method to solve a second order  ordinary differential equation with a nonzero RHS
 The d.e is of the form :  - d/dx(  p(x)dy/dx) + q(x)y = f(x).
 The integration is performed on the interval 0<=x<=1'''
 
@@ -15,7 +15,7 @@ x = np.linspace(0,1, N)
 def basis_function(x):
     '''Basis functions of the Rayleigh-Ritz method'''
     h = []                                  #intialize space step
-    phi = np.zeros([N, N])                  #initialize basis function
+    phi = np.zeros([N, N], dtype='float')                  #initialize basis function
     for i in range(N-1):
         h.append(x[i+1] - x[i])
     for j in range(1, N-1):
@@ -30,3 +30,23 @@ def basis_function(x):
                 phi[j, idx] = 0
     return phi
 #basis_function(x)
+
+#coefficient functions 
+def p(x):
+    '''approximating the coefficient p in the d.e.'''
+    pass
+
+def q(x):
+    '''approximating the coefficient q in the d.e.'''
+    pass
+#initialize vectors for computing integrals
+Q1 = np.zeros(N, dtype='float')
+Q2 = np.zeros(N, dtype='float')
+Q3 = np.zeros(N, dtype='float')
+Q4 = np.zeros(N, dtype='float')
+Q5 = np.zeros(N, dtype='float')
+Q6 = np.zeros(N, dtype='float')
+#approximating the 6 integrals
+for i in range(N-1):
+    pass
+
