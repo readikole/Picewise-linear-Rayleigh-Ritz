ae71351838216db120d4bdafe31951b07dfa39fb
Realeboga Dikole <30493588+readikole@users.noreply.github.com>
2021-01-21 15:49:31 +0200

    Add files via upload

--- Rayleigh-Ritz Method.py
+++ Rayleigh-Ritz Method.py
@@ -8,45 +8,69 @@ The integration is performed on the interval 0<=x<=1'''
 def rhs(x):
     '''This computes the rhs of the d.e.
     INPUT: the variable x'''
-    return np.cos(x)
-N =100
-x = np.linspace(0,1, N)
+    return np.sin(np.pi*x)
+N =5
+x = 2*np.pi*np.linspace(0,1, N)
+
+
+h = []          #intialize space step
+for i in range(N-1):
+        h.append(x[i+1] - x[i])
 
 def basis_function(x):
     '''Basis functions of the Rayleigh-Ritz method'''
-    h = []                                  #intialize space step
-    phi = np.zeros([N, N], dtype='float')                  #initialize basis function
-    for i in range(N-1):
-        h.append(x[i+1] - x[i])
-    for j in range(1, N-1):
+                                    
+    phi = np.zeros([N, N], dtype='float')                               #initialize basis function
+    for j in range(1, N-1):             
         for idx in range(1, N-1):
             if (0<=x[idx]<=x[j-1]):
                 phi[j, idx] = 0
-            elif (x[j-1]<=x[idx]<=x[j]):
-                phi[j, idx] = (x[idx] - x[idx-1])/h[idx-1]
-            elif (x[j]<=x[idx]<=x[j+1]):
-                phi[j, idx] = ((x[idx+1] - x[idx])/h[idx])
-            elif (x[j+1]<x[idx]<1):
+            elif (x[j-1]<x[idx]<=x[j]):
+                phi[j, idx] = (x[idx] - x[j-1])/h[j-1]
+            elif (x[j]<x[idx]<=x[j+1]):
+                phi[j, idx] = ((x[j+1] - x[idx])/h[j])
+            elif (x[j+1]<x[idx]<=1):
                 phi[j, idx] = 0
     return phi
 #basis_function(x)
 
 #coefficient functions 
 def p(x):
-    '''approximating the coefficient p in the d.e.'''
-    pass
+    '''Defining the coefficient p in the d.e.'''
+    return 1
 
 def q(x):
-    '''approximating the coefficient q in the d.e.'''
-    pass
+    '''Defining the coefficient q in the d.e.'''
+    return np.pi**2
 #initialize vectors for computing integrals
-Q1 = np.zeros(N, dtype='float')
-Q2 = np.zeros(N, dtype='float')
-Q3 = np.zeros(N, dtype='float')
-Q4 = np.zeros(N, dtype='float')
-Q5 = np.zeros(N, dtype='float')
-Q6 = np.zeros(N, dtype='float')
+Q1 = []
+Q2 = []
+Q3 = []
+Q4 = []
+Q5 = []
+Q6 = []
 #approximating the 6 integrals
 for i in range(N-1):
-    pass
+    Q1.append((h[i]/12)*(p(x[i]) + q(x[i+1])))
+    Q2.append((h[i-1]/12)*(3*q(x[i]) + q(x[i-1])))
+    Q3.append((h[i])*(3*q(x[i]) + q(x[i+1])))
+    Q4.append((h[i-1]/2)*(p(x[i]) + q(x[i-1])))
+    Q5.append((h[i-1]/6)*(2*rhs(x[i]) + rhs(x[i-1])))
+    Q6.append((h[i]/6)*(2*rhs(x[i]) + rhs(x[i+1])))
+    #now compute  Q4,n 
+    Q4.append((h[N-2]/2)*(p(x[N-1]) + q(x[N-2])))
+
+alpha = np.zeros(N, dtype='float')
+beta = np.zeros(N, dtype='float')
+b = np.zeros(N, dtype='float')
+a = np.zeros(N, dtype='float')
+for i in range(N-2):
+    alpha[i] = Q4[i] + Q4[i+1] + Q2[i] + Q3[i]
+    beta[i] = Q1[i] - Q4[i+1]
+    b[i] = Q5[i] + Q6[i]
+
+print(len(Q4))
+alpha[N-1] = Q4[N-1] + Q4[N] + Q2[N-1] + Q3[N-1]
+
+
 
