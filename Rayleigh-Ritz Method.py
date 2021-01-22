4764b4fbba712718b4ed89c3020af2e60afb73bb
Realeboga Dikole <30493588+readikole@users.noreply.github.com>
2021-01-22 12:28:06 +0200

    Add files via upload
    Code not working properly at the moment --- not yielding the right result

--- Rayleigh-Ritz Method.py
+++ Rayleigh-Ritz Method.py
@@ -9,18 +9,21 @@ def rhs(x):
     '''This computes the rhs of the d.e.
     INPUT: the variable x'''
     return np.sin(np.pi*x)
-N =5
-x = 2*np.pi*np.linspace(0,1, N)
+N =10
+x = 2*np.pi*np.linspace(0,1, N+1)
 
 
 h = []          #intialize space step
+h0 = x[1] - x[0]
+h.append(h0)
 for i in range(N-1):
         h.append(x[i+1] - x[i])
-
+hN = x[N] - x[N-1]
+h.append(hN)
 def basis_function(x):
     '''Basis functions of the Rayleigh-Ritz method'''
                                     
-    phi = np.zeros([N, N], dtype='float')                               #initialize basis function
+    phi = np.zeros([N+1, N+1], dtype='float')                               #initialize basis function
     for j in range(1, N-1):             
         for idx in range(1, N-1):
             if (0<=x[idx]<=x[j-1]):
@@ -51,26 +54,72 @@ Q5 = []
 Q6 = []
 #approximating the 6 integrals
 for i in range(N-1):
-    Q1.append((h[i]/12)*(p(x[i]) + q(x[i+1])))
-    Q2.append((h[i-1]/12)*(3*q(x[i]) + q(x[i-1])))
-    Q3.append((h[i])*(3*q(x[i]) + q(x[i+1])))
-    Q4.append((h[i-1]/2)*(p(x[i]) + q(x[i-1])))
-    Q5.append((h[i-1]/6)*(2*rhs(x[i]) + rhs(x[i-1])))
-    Q6.append((h[i]/6)*(2*rhs(x[i]) + rhs(x[i+1])))
-    #now compute  Q4,n 
-    Q4.append((h[N-2]/2)*(p(x[N-1]) + q(x[N-2])))
+    q1 = (h[i]/12)*(p(x[i]) + q(x[i+1]))
+    Q1.append(q1)
+    q2 = (h[i-1]/12)*(3*q(x[i]) + q(x[i-1]))
+    Q2.append(q2)
+    q3 = (h[i])*(3*q(x[i]) + q(x[i+1]))
+    Q3.append(q3)
+    q4 = (h[i-1]/2)*(p(x[i]) + q(x[i-1]))
+    Q4.append(q4)
+    q5 = (h[i-1]/6)*(2*rhs(x[i]) + rhs(x[i-1]))
+    Q5.append(q5)
+    q6 = (h[i]/6)*(2*rhs(x[i]) + rhs(x[i+1]))
+    Q6.append(q6)
+#now compute  Q1,n , Q2,n , Q3,n , Q4,n Q4,n+1, Q5n, Q6,n
+
+q1n = (h[N-1]/12)*(p(x[N-1]) + q(x[N]))
+Q1.append(q1n)
+q2n = (h[N-1]/12)*(3*q(x[N]) + q(x[N-1]))
+Q2.append(q2n)
+q3n = (h[N-1])*(3*q(x[N-1]) + q(x[N]))
+Q3.append(q3n)
+q4n_last = (h[N]/2)*(p(x[N]) + q(x[N-1]))
+q4n_second_last = (h[N-1]/2)*(p(x[N-1]) + q(x[N-2]))
+Q4.append(q4n_second_last)
+Q4.append(q4n_last)
+q5n = (h[N-1]/6)*(2*rhs(x[N]) + rhs(x[N-1]))
+Q5.append(q5n)
+q6n = (h[N-1]/6)*(2*rhs(x[N-1]) + rhs(x[N]))
+Q6.append(q6n)
+
 
 alpha = np.zeros(N, dtype='float')
 beta = np.zeros(N, dtype='float')
 b = np.zeros(N, dtype='float')
 a = np.zeros(N, dtype='float')
-for i in range(N-2):
+for i in range(N-1):
     alpha[i] = Q4[i] + Q4[i+1] + Q2[i] + Q3[i]
     beta[i] = Q1[i] - Q4[i+1]
     b[i] = Q5[i] + Q6[i]
-
-print(len(Q4))
 alpha[N-1] = Q4[N-1] + Q4[N] + Q2[N-1] + Q3[N-1]
+b[N-1] = Q5[N-1] + Q6[N-1]
+a[0] = alpha[0]
 
+#solve a symmetric tridiagonal linear system 
+zeta = np.zeros(N+1, dtype='float')
+z = np.zeros(N+1, dtype='float')
+c = np.zeros(N+1, dtype='float')                      #the weights
+zeta[0] = beta[0]/alpha[0]
+z[0] = b[0]/a[0]
+for i in range(1, N-1):
+    a[i] = alpha[i] - beta[i-1]*zeta[i-1]
+    zeta[i] = beta[i]/alpha[i]
+    z[i] = (b[i] - beta[i-1]*z[i-1])/a[i]
+a[N-1] = alpha[N-1] - beta[N-2]*zeta[N-2]
+z[N-1] = (b[N-1] - beta[N-2]*z[N-2])/a[N-1]
 
-
+#indices for the weights
+#indices must be reversed
+indices = []
+for i in range(N):
+    a =  N-1-i
+    indices.append(a)
+print(indices)
+c[N-1] = z[N-1]
+print(c.shape)
+for i in indices:
+    c[i] = z[i] - zeta[i]*c[i+1]
+u = basis_function(x)
+phi_new = np.dot(u, c)
+print(phi_new)
